2dce2b03425d47c4eef5bd6bb04675788908fcb2
nf <nf@wh3rd.net>
2023-04-12 12:50:50 +1000

    nux: use return code when exiting

--- main.go
+++ main.go
@@ -43,26 +43,28 @@ type Device struct{}
 func (Device) In(byte) byte        { panic("device input not implemented") }
 func (Device) InShort(byte) uint16 { panic("device input not implemented") }
 
-func (Device) Out(device, v byte) {
-	switch device {
+func (Device) Out(d, v byte) {
+	switch d {
 	case 0x0f:
-		if v == 0x01 {
-			os.Exit(0)
+		if v != 0 {
+			os.Exit(int(0x7f & v))
 		}
-		panic(fmt.Errorf("device 0x0f given unknown value %x", v))
 	case 0x18:
 		os.Stdout.Write([]byte{v})
+	default:
+		panic(fmt.Errorf("device %x not implemented", d))
 	}
 }
 
-func (Device) OutShort(device byte, v uint16) {
-	switch device {
+func (Device) OutShort(d byte, v uint16) {
+	switch d {
 	case 0x0f:
-		if v == 0x01 {
-			os.Exit(0)
+		if v != 0 {
+			os.Exit(int(0x7f & v))
 		}
-		panic(fmt.Errorf("device 0x0f given unknown value %x", v))
 	case 0x18:
 		os.Stdout.Write([]byte{byte(v >> 8), byte(v)})
+	default:
+		panic(fmt.Errorf("device %x not implemented", d))
 	}
 }
